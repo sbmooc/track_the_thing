c01d7115b204a45f7ff25aa73b2e0dcf9904b72e
benkirby <ben.kirby14@hotmail.com>
2019-06-14 18:06:27 +0100

    Tracker info table added to tracker page, plus sorted out header bar stickiness

--- tcr_tracker/tracker/views.py
+++ tcr_tracker/tracker/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
-
 from tcr_tracker.tracker.models import Trackers, Riders
+
+
 
 
 class AllTrackers(ListView):
@@ -13,10 +14,11 @@ class AllRiders(ListView):
     model = Riders
 
 
-# class OneTracker(TemplateView):
-#     template_name = "tracker.html"
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['tracker_id'] = Trackers.objects.all()
-#         return context
+class OneTracker(TemplateView):
+    template_name = "tracker.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(OneTracker, self).get_context_data(**kwargs)
+        context['dummy_field_list'] = ["esn_number", "working_status", "loan_status", "last_test_date", "purchase_date", "warranty_expiry",
+                  "owner"]
+        return context
--- tcr_tracker/urls.py
+++ tcr_tracker/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from tcr_tracker.tracker import views
-from tcr_tracker.tracker.views import AllTrackers, AllRiders
+from tcr_tracker.tracker.views import AllTrackers, AllRiders, OneTracker
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trackers/', AllTrackers.as_view()),
     path('riders/', AllRiders.as_view()),
-    path('tracker/', TemplateView.as_view(template_name="tracker.html")),
+    path('tracker/', OneTracker.as_view()),
     # path('riders/<int:rider_id>', views.rider)
 ]
