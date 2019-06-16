1ecc1c207e3c215f7b33e4542dfddc1e89c3fb87
Oli M-C <oliver.monaghan-coombs@plentific.com>
2019-06-16 14:28:00 +0100

    Views template forms (#3)
    * Use detail view for trackers
    
    * Pass tracker data through context
    
    * Basic tracker edit form added
    
    * Basic edit form for riders
    
    * added timestamp model
    
    * added migration
    
    * update

--- tcr_tracker/tracker/views.py
+++ tcr_tracker/tracker/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, DetailView, UpdateView
+
+from tcr_tracker.forms import EditTracker, EditRider
 from tcr_tracker.tracker.models import Trackers, Riders
-
-
 
 
 class AllTrackers(ListView):
     model = Trackers
 
 
+class OneTracker(DetailView):
+    model = Trackers
+
+    def get_context_data(self, **kwargs):
+        context = super(OneTracker, self).get_context_data(**kwargs)
+        context['tracker_dict'] = context['trackers'].__dict__
+        context['tracker_dict'].pop('_state')
+        return context
+
+
+class TrackerEdit(UpdateView):
+    model = Trackers
+    form_class = EditTracker
+    template_name = 'tracker/trackers_edit.html'
+
+
+class RiderEdit(UpdateView):
+    model = Riders
+    form_class = EditRider
+    template_name = 'tracker/riders_edit.html'
+
+
 class AllRiders(ListView):
     model = Riders
 
 
-class OneTracker(TemplateView):
-    template_name = "tracker.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(OneTracker, self).get_context_data(**kwargs)
-        context['dummy_field_list'] = ["esn_number", "working_status", "loan_status", "last_test_date", "purchase_date", "warranty_expiry",
-                  "owner"]
-        return context
+class OneRider(DetailView):
+    model = Riders
--- tcr_tracker/urls.py
+++ tcr_tracker/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
-from tcr_tracker.tracker import views
-from tcr_tracker.tracker.views import AllTrackers, AllRiders, OneTracker
-
+from tcr_tracker.tracker.views import (
+    AllTrackers,
+    AllRiders,
+    OneTracker,
+    OneRider,
+    TrackerEdit,
+    RiderEdit,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('trackers/', AllTrackers.as_view()),
-    path('riders/', AllRiders.as_view()),
-    path('tracker/', OneTracker.as_view()),
-    # path('riders/<int:rider_id>', views.rider)
+    path('riders/', AllRiders.as_view(), name='all_riders'),
+    path('riders/<int:pk>', OneRider.as_view(), name='one_rider'),
+    path('riders/<int:pk>/edit', RiderEdit.as_view(), name='rider_edit'),
+    path('trackers/', AllTrackers.as_view(), name='all_trackers'),
+    path('trackers/<int:pk>', OneTracker.as_view(), name='one_tracker'),
+    path('trackers/<int:pk>/edit', TrackerEdit.as_view(), name='tracker_edit'),
 ]
