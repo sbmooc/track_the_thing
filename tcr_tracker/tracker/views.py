530f03f1908c3967a931f4030056b4638b940ee7
Oli M-C <oliver.monaghan-coombs@plentific.com>
2019-06-16 11:13:41 +0100

    Single tracker rider page (#2)
    * Made assignment/possession optional fields for trackers
    
    * Changed search bar behaviour to have small search icon always on right. Need to figure out how to have tags on same *small* line as well as large
    
    * Removed side bar and button, got tags & search bar responding properly at top of page.
    
    * Table view of trackers completed, with proof of concept of "assignable" column disappearing below a certain screen size.
    
    * Riders page added with same format as trackers
    
    * Footer navbar added to base.html
    
    * Static tracker page created, with statuses at top of page
    
    * Added hard-coded links to enable nav, added event buttons to tracker page
    
    * Notes list added to tracker page
    
    * Event log added to tracker page
    
    * Tracker info table added to tracker page, plus sorted out header bar stickiness

--- tcr_tracker/tracker/views.py
+++ tcr_tracker/tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
-
+from django.http import HttpResponse
+from django.views.generic import ListView, TemplateView
 from tcr_tracker.tracker.models import Trackers, Riders
+
+
 
 
 class AllTrackers(ListView):
@@ -11,3 +13,12 @@ class AllTrackers(ListView):
 class AllRiders(ListView):
     model = Riders
 
+
+class OneTracker(TemplateView):
+    template_name = "tracker.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(OneTracker, self).get_context_data(**kwargs)
+        context['dummy_field_list'] = ["esn_number", "working_status", "loan_status", "last_test_date", "purchase_date", "warranty_expiry",
+                  "owner"]
+        return context
--- tcr_tracker/urls.py
+++ tcr_tracker/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 from tcr_tracker.tracker import views
-from tcr_tracker.tracker.views import AllTrackers, AllRiders
+from tcr_tracker.tracker.views import AllTrackers, AllRiders, OneTracker
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trackers/', AllTrackers.as_view()),
     path('riders/', AllRiders.as_view()),
-    # path('trackers/<int:tracker_id>', views.tracker),
+    path('tracker/', OneTracker.as_view()),
     # path('riders/<int:rider_id>', views.rider)
 ]
