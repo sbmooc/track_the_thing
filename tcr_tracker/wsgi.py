6b62f626e6ee04e4fe32b4b0367408cdfa8f5159
Oli m-c <oliver.monaghan-coombs@plentific.com>
2019-07-07 09:59:57 +0100

    added sentry

--- tcr_tracker/wsgi.py
+++ tcr_tracker/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+import sentry_sdk
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tcr_tracker.settings')
 
 application = get_wsgi_application()
+if settings.SENTRY_API_KEY:
+    sentry_sdk.init(settings.SENTRY_API_KEY)
