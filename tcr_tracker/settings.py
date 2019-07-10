d96ac04869ad1ad6d0805bc0d565e0b8f94459e6
oli <hello@olimc.co.uk>
2019-07-10 07:25:35 +0100

    added white noise to serve static files

--- tcr_tracker/settings.py
+++ tcr_tracker/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +135,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/trackers'
 
 SENTRY_API_KEY = os.environ.get('SENTRY_API', None)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
