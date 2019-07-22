8cb84bcd8ee7701509f970909f564b99bb7ab357
Oli m-c <oliver.monaghan-coombs@plentific.com>
2019-07-22 15:49:52 +0100

    added track3r to allowed hosts

--- tcr_tracker/settings.py
+++ tcr_tracker/settings.py
@@ -47,7 +47,7 @@ else:
 DEBUG = False if ENVIRONMENT == 'prod' else True
 
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['track3r.co.uk', 'www.track3r.co.uk', '.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
