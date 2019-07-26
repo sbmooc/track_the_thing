24e07c6b400f14b36261279b19b67a16ce7122c7
Oli m-c <oliver.monaghan-coombs@plentific.com>
2019-07-26 07:45:01 +0300

    mgmt command for riders and trackers

--- tcr_tracker/tracker/management/commands/downloadrider_trackers.py
+++ tcr_tracker/tracker/management/commands/downloadrider_trackers.py
@@ -2,7 +2,7 @@ import csv
 
 from arrow import arrow
 from django.core.management.base import BaseCommand, CommandError
-from tcr_tracker.tracker.models import Rider, Deposit, Event
+from tcr_tracker.tracker.models import Rider, Deposit, Event, Tracker
 
 
 class Command(BaseCommand):
@@ -13,43 +13,30 @@ class Command(BaseCommand):
         datetime = arrow.Arrow.now().format("DD-MM-YY HH:MM")
         with open('rider_tracker_download_'+datetime, 'w', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(
-                'Rider TCR_ID',
+            writer.writerow([
+                'Tracker TCR ID',
+                'Tracker ESN Number',
                 'Rider Cap Number',
                 'Rider Full Name',
-                'Trackers assigned to rider',
-                'Trackers in possession by rider',
-
-
+                'Tracker URL (if rider\'s own tracker',
+                ]
             )
-            for rider in Rider.objects.filter(status='active').order_by('display_order'):
-                number_of_trackers_assigned = len(rider.trackers_assigned.all())
-                if number_of_trackers_assigned > 1:
-                    print(f'Rider {rider.cap_number} {rider.full_name} has more than one'
-                          f'tracker assigned')
-                if number_of_trackers_assigned < 1:
-                    print(f'Rider {rider.cap_number} {rider.full_name} has less than one'
-                          f'tracker assigned')
-
+            # warn if riders have multiple trackers or no trackers
+            for rider in Rider.objects.filter(status='active'):
+                if len(rider.trackers_assigned.all()) != 1:
+                    print(f'Rider {rider.cap_number} {rider.full_name} has '
+                          f'{len(rider.trackers_assigned.all())} trackers assigned!')
+            for tracker in Tracker.objects.filter(
+                    rider_assigned__isnull=False,
+                    rider_assigned__status='active'
+            ):
 
                 writer.writerow(
                     [
-                        rider.tcr_id,
-                        rider.cap_number,
-                        rider.full_name,
-                        [
-                            f'{tracker.tcr_id}: '
-                            f'{tracker.esn_number}'
-                            f'' for tracker in rider.trackers_assigned.all()
-                        ],
-                        [
-                            tracker.esn_number for tracker in rider.trackers_possessed.all()
-                        ],
-                        [
-                            tracker.tcr_id for tracker in rider.trackers_possessed.all()
-                        ],
-                        rider.tracker_url,
-                        len(rider.trackers_possessed.all())
-
+                        tracker.tcr_id,
+                        tracker.esn_number,
+                        tracker.rider_assigned.cap_number,
+                        tracker.rider_assigned.full_name,
+                        tracker.rider_assigned.tracker_url
                     ]
                 )
